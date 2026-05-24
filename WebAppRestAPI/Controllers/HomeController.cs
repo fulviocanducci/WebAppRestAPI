c3ab7cf46edc000014aa41bf7c50f7bb06b6f579
Fulvio Cezar Canducci Dias <fulviocanducci@hotmail.com>
2026-05-24 10:44:48 -0300

    Add project files.

---
+++ WebAppRestAPI/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public HomeController() { }
+
+        [HttpGet]
+        public IActionResult GetHome()
+        {
+            object data = new
+            {
+                Date = DateTime.Now,
+            };
+            return Ok(data);
+        }
+    }
+}
